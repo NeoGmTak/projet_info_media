8d70817949e9ed5382726abfc95acef6a01c44ae
Ju' Boilevin <jumortondemmel@gmail.com>
2017-10-31 10:48:08 +0100

    Ajout du fichier template.css avec les fontfaces

--- libs.php
+++ libs.php
@@ -27,6 +27,7 @@ function html_header($title, $style = array())
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 		
 		<!-- Styles communs -->
+		<link rel="stylesheet" type="text/css" href="./css/template.css">
 		<link rel="stylesheet" type="text/css" href="./css/style.css">
 		
 		<!-- Style spécifique -->
