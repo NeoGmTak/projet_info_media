aa9cbeb74863f4146d4372ef7b6032cf6a802728
Maxime <maxime.fauchille@gmail.com>
2017-11-03 11:35:02 +0100

    Dev page ou manger

--- requete.php
+++ requete.php
@@ -3,9 +3,9 @@
 include 'connexionBDD.php';
 
 $where = ($_GET['typeEtablissement'] == 1) ? ' WHERE 1' : ' WHERE typeEtablissement ='.$_GET['typeEtablissement'];
-$reponse = $bdd->query('SELECT nom FROM OuManger'.$where);
+$reponse = $bdd->query('SELECT typeEtablissement, nom, description, urlImage FROM OuManger'.$where);
 while($donnee = $reponse->fetch()) {
-    echo $donnee['nom'];
+    echo "<img src=\"".$donnee['urlImage']."\" alt=\"image établissement restauration\" class=\"imgEtablissementResto\"> <br> <h4 class=\"typeEtabRestau\">".strtoupper($donnee['typeEtablissement'])."</h4> <h3 class=\"nomEtabRestau\">".$donnee['nom']."</h3><p class=\"descRestau\">".$donnee['description']."</p><br>;";
 }
 
 ?>
