98f6818240e4ad74e731d9467aaac0e5e4d60da8
Maxime <maxime.fauchille@gmail.com>
2017-12-07 22:21:28 +0100

    ajout redirection mail

--- mail.php
+++ mail.php
@@ -11,16 +11,13 @@
     $body="$message \n\n$headers";
 
     if(mail($dest,$subject,$body)){
-    ?>
-<div class="validation">
-    <p align="center">Votre mail a bien été envoyé, merci de votre retour.<br></p>
-</div>
-<?php
-    }else{
+        ?><div class="validation"><p align="center" >Votre mail a été envoyé<br></p>
+            <p>Vous allez bientot etre redirigé vers la page d'acceuil<br>
+                Si vous n'etes pas redirigé au bout de 3 secondes cliquez <a href="http://grangepro.ch/vieux-lyon/index.php">ici</a></p></div><?php
+        }else{
+            ?><div class="validation"><p align="center" >Une erreur s'est produite<br></p>
+        <p>Vous allez bientot etre redirigé vers la page d'acceuil<br>
+                Si vous n'etes pas redirigé au bout de 3 secondes cliquez <a href="http://grangepro.ch/vieux-lyon/index.php">ici</a></p></div><?php
+        }
         ?>
-    <div class="validation">
-        <p align="center">Une erreur s'est produite. Veuillez-nous excuser pour la gêne occasionnée.<br></p>
-    </div>
-    <?php
-    }
-    ?>
+        <META http-equiv="refresh" content="3; URL=http://grangepro.ch/vieux-lyon/index.php"></META>
